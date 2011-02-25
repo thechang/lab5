1c0db738332207b242e235222d12b722defa974c
thechang <guschang180@hotmail.com>
2011-02-24 23:21:16 -0600

    ultimo commit de lab5

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,21 @@ class Product < ActiveRecord::Base
     validates_numericality_of :projection, :greater_than => 0
     validates_uniqueness_of :title
     validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i, :message => "Only letters allowed"
+    
+    def before_create
+	id = product.id
+	product_id = product.product_id
+	previous_balance = 0 
+	new_balance = product.amount 
+	future_balance = product.amount + product.projection 
+    end
+    def before_modifying
+	previous_balance = product.new_balance 
+	new_balance = product.amount 
+	future_balance = product.amount + product.projection
+    end
+    def before_delete
+	delete from inventory where inventory.id=product.id
+    end
+    
 end
